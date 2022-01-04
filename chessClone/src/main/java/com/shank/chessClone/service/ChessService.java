40b683d906aa8ecef9420d7f5305644eb60e0f6f
Alex Shank <ashank31@gmail.com>
2022-01-04 16:31:16 -0600

    Removed chess clone that I never actually worked on :)

--- chessClone/src/main/java/com/shank/chessClone/service/ChessService.java
+++
@@ -1,8 +0,0 @@
-package com.shank.chessClone.service;
-
-import com.shank.chessClone.model.BoardSquare;
-import com.shank.chessClone.model.ChessBoard;
-
-public interface ChessService {
-    ChessBoard movePiece(ChessBoard board, BoardSquare source, BoardSquare destination);
-}
